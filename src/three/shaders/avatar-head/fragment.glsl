9c3fe2d46bd300a5f486505c31cc125103b07810
Jorge Rodriguez <tankers76@hotmail.com>
2026-05-24 20:04:56 +0200

    Revert "feat: render avatar with bald head"
    This reverts commit dde27530a2ecd458b10e7322a740304db16bca0b.

--- src/three/shaders/avatar-head/fragment.glsl
+++ src/three/shaders/avatar-head/fragment.glsl
@@ -8,10 +8,6 @@ varying vec2 vUv;
 
 void main() {
     vec4 tex = texture2D(uHeadTexture, vUv);
-    float luminance = dot(tex.rgb, vec3(0.299, 0.587, 0.114));
-    float hairMask = smoothstep(0.34, 0.16, luminance);
-    vec3 scalpColor = mix(vec3(0.68, 0.42, 0.31), vec3(0.95, 0.68, 0.50), smoothstep(0.05, 0.34, luminance));
-    tex.rgb = mix(tex.rgb, scalpColor, hairMask);
 
     float progress = getProgress();
 
